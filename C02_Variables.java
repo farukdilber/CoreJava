cbe3bdf4941f13eb0d5dae5f345310890275764f
Faruk Dilber <fdilberx@gmail.com>
2021-11-18 10:23:44 +0300

    Add files via upload

---
+++ C02_Variables.java
@@ -0,0 +1,15 @@
+package day01_02Variables;
+
+public class C02_Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String okul="Yildiz Koleji" ;
+      okul="Ankara Koleji";
+      System.out.println(okul);
+      
+      System.out.println("Okul adi : " + okul);
+      System.out.println("Okul adi : " + "okul");
+	}
+
+}
