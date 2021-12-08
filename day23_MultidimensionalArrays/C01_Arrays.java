1cf37ad7e68dfb691b56919753daf17fea874cad
Faruk Dilber <fdilberx@gmail.com>
2021-12-08 18:09:05 +0300

    Add files via upload

---
+++ day23_MultidimensionalArrays/C01_Arrays.java
@@ -0,0 +1,12 @@
+package day23_MultidimensionalArrays;
+
+public class C01_Arrays {
+
+	public static void main(String[] args) {
+		// Verilen bir arrrayden istenen degere sahip elemenlerti silip
+		// kalani yeni bir array olarak kaydedin ve sonra yeni arrayi yazdir
+		
+
+	}
+
+}
