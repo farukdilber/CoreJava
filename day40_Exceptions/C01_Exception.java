441fc437b694e939a32c4ea257867d7e1496a484
Faruk Dilber <fdilberx@gmail.com>
2021-12-24 20:43:50 +0300

    Add files via upload

---
+++ day40_Exceptions/C01_Exception.java
@@ -0,0 +1,26 @@
+package day40_Exceptions;
+
+public class C01_Exception {
+public static void main(String[] args) {
+		
+		int a=20;
+		int b=0;
+		
+		
+	
+		try {
+			System.out.println("sayilarin bolumu : " + a/b); 
+			
+		}catch (ArithmeticException e) {
+			
+			System.out.println("Sifira bolum yapilamaz");
+			// System.out.println(e.getMessage()); //    / by zero  exception'in kaynagini
+			e.printStackTrace();
+			// java.lang.ArithmeticException: / by zero
+			//		at day40.C01_exception.main(C01_exception.java:12)
+		}
+
+		System.out.println("Gorev tamamlandi");
+
+	}
+}
