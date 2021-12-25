6c6423cab6ff102bd349863dbf7a6b6531afbdb8
Faruk Dilber <fdilberx@gmail.com>
2021-12-25 18:56:34 +0300

    Add files via upload

---
+++ day41_Errors_GarbageCollector/C01_Final.java
@@ -0,0 +1,11 @@
+package day41_Errors_GarbageCollector;
+
+public class C01_Final {
+	final static double pi=3.14;
+
+	public static void main(String[] args) {
+		
+		System.out.println(pi);
+
+	}
+}
