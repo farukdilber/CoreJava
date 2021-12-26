1a9b79d43249659acb0742f31e5136d96cbf35a6
Faruk Dilber <fdilberx@gmail.com>
2021-12-26 11:44:35 +0300

    Add files via upload

---
+++ day42_AbstractClasses/Isci.java
@@ -0,0 +1,7 @@
+package day42_AbstractClasses;
+
+public class Isci extends Muhasebe {
+	public void mesai() {
+		System.out.println("Her iscinin mesai saatleri belli olmali. Imza Isci");
+	}
+}
---
+++ day42_AbstractClasses/Personel.java
@@ -0,0 +1,7 @@
+package day42_AbstractClasses;
+
+public class Personel {
+	public void statu() {
+		System.out.println("Sirkete alinan herkes bizim personelimizdir .. imza:Persdonel");
+	}
+}
