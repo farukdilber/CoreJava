4e913f42f41006666f11445c3bd6ffc0e9759600
Faruk Dilber <fdilberx@gmail.com>
2022-01-04 09:26:28 +0300

    Add files via upload

---
+++ d12_Varargs/Q01.java
@@ -0,0 +1,10 @@
+package d12_Varargs;
+
+public class Q01 {
+	public static void main(String[] args) {
+		//  verilen Stringleri birlestiren concat isimli bir method olusturunuz
+		// input : "m", "e", "r", "v", "e";
+		// output : merve
+
+	}
+}
